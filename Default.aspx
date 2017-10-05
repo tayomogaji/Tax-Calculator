ec3340e3432d080612b502353b1a1784a4077d5a
tayomogaji <mattayo@live.co.uk>
2017-10-05 12:28:06 +0100

    Add files via upload

---
+++ Default.aspx
@@ -0,0 +1,108 @@
+﻿<%@ Page Language="C#" AutoEventWireup="true" CodeFile="Default.aspx.cs" Inherits="Tax" %>
+
+<!DOCTYPE html>
+<html xmlns="http://www.w3.org/1999/xhtml">
+    
+<head runat="server">
+    <title>Tax Calculator</title>
+    <link rel="shortcut icon" type="image/x-icon" href="calculator_icon_fxx_icon.ico" />
+    <link href="../Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="taxStyle.css" rel="stylesheet" type="text/css"/>
+</head>
+
+<body>
+    <div class="jumbotron">
+    <h1>Tax calculator</h1>
+    </div>
+
+    <%--------------------------start of form section-------------------------%>
+    <form id="form1" runat="server">   
+    <div class="w3-container" id="mainInput" runat="server">
+      <div class="row">
+     <%--------------------------start of Input section-------------------------%>
+          <div class="col-md-4 col-md-offset-1 calculate">
+
+            <%--Dropdown and list items for Income and time period.--%> 
+            <hr/><h2>Income</h2><hr/>
+            <asp:TextBox class="options" ID="userIncome" runat="server" Placeholder="Your Income"></asp:TextBox>
+            <div>
+            <h4>per</h4>
+            <asp:DropDownList class="options" ID="period" runat="server" OnSelectedIndexChanged="period_SelectedIndexChanged">
+                <asp:ListItem Value ="Y">Year</asp:ListItem>
+                <asp:ListItem Value ="M">Month</asp:ListItem>
+                <asp:ListItem Value ="W">Week</asp:ListItem>
+            </asp:DropDownList>
+            <br/>
+            <asp:Label ID="errortext" runat="server" Text=""></asp:Label>
+        </div><hr/>
+
+        <div>
+            <h2>Age</h2><hr/>
+            <asp:DropDownList class="options" ID="age" runat="server">
+                <asp:ListItem Value ="U">U65</asp:ListItem>
+                <asp:ListItem Value ="O">65+</asp:ListItem>
+            </asp:DropDownList>
+
+            <%--National Insureance checkbox option--%>
+            <h4>Include NI</h4>
+            <asp:CheckBox class="options" ID="ni" runat="server" Checked="True" />
+        </div><hr/>
+
+        <div>
+        <asp:Button class="options" ID="calculate" runat="server" Text="Calculate" OnClick="calculate_Click" />
+        </div>
+        </div>
+    <%--------------------------end of form section-------------------------%>
+
+    <%------------------------start of Results section-------------------------%>
+          <div class="col-md-5 col-md-offset-1 tax">
+        <div class="afterTax">
+            <h2>After Tax</h2><hr/>
+        <asp:Label ID="afterTax" runat="server" Text=""></asp:Label><hr/>
+        <h4>Breakdown</h4><br/> 
+            </div>
+            
+
+        <div class="col-md-7 col-md-offset-2 text-left">
+        <asp:Table ID="breakdown" runat="server" Width="279px"> 
+            <asp:TableRow>
+            <asp:TableCell>Gross Income</asp:TableCell> 
+                <asp:TableCell><asp:Label ID="gross" runat="server" Text=""></asp:Label></asp:TableCell> 
+        </asp:TableRow>
+
+            <asp:TableRow>
+            <asp:TableCell>Allowance</asp:TableCell>
+                <asp:TableCell><asp:Label ID="allowance" runat="server" Text=""></asp:Label></asp:TableCell> 
+        </asp:TableRow>
+
+            <asp:TableRow>
+            <asp:TableCell>Taxable Income</asp:TableCell>
+                <asp:TableCell><asp:Label ID="taxableTotal" runat="server" Text=""></asp:Label></asp:TableCell> 
+        </asp:TableRow>
+
+            <asp:TableRow>
+            <asp:TableCell>Taxed Income</asp:TableCell>
+                <asp:TableCell><asp:Label ID="taxed" runat="server" Text=""></asp:Label></asp:TableCell> 
+        </asp:TableRow>
+
+            <asp:TableRow>
+            <asp:TableCell>National Insurance</asp:TableCell>
+                <asp:TableCell><asp:Label ID="niTax" runat="server" Text=""></asp:Label></asp:TableCell> 
+        </asp:TableRow>
+
+            <asp:TableRow>
+            <asp:TableCell>Total Tax Due</asp:TableCell>
+                <asp:TableCell><asp:Label ID="taxTotal" runat="server" Text=""></asp:Label></asp:TableCell> 
+        </asp:TableRow>
+    </asp:Table> 
+            </div>
+            </div>
+            
+                    <%--using a table to breakdown the details--%>
+        
+     <%------------------------end of Results section-------------------------%>
+   </div><%--row--%>
+ </div><%--w3-container--%>
+</form>
+</body>
+</html>
---
+++ Default.aspx.cs
@@ -0,0 +1,173 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+public partial class Tax : System.Web.UI.Page
+{
+    protected void Page_Load(object sender, EventArgs e)
+    {
+
+    }
+
+    protected void period_SelectedIndexChanged(object sender, EventArgs e)
+    {
+        
+    }
+
+    protected void calculate_Click(object sender, EventArgs e)
+    {
+        try
+        {
+            //declearing total taxable income.
+            double income = Convert.ToDouble(userIncome.Text);
+
+            /* declearing variables for tax allowance and income margins.
+             defult set to year */
+            double taxFree = 11500;
+            double highIncome = 45000;
+            double addIncome = 150000;
+
+
+            //optional month and week time frames included in switch satatment
+            string timeFrame = (period.SelectedValue);
+            double month = 12;
+            double week = 52;
+
+
+            switch (timeFrame)
+            {
+                //per month
+                case "M":
+                    taxFree = Math.Round(11500 / month, 2);
+                    highIncome = Math.Round(45000 / month, 2);
+                    addIncome = Math.Round(150000 / month, 2);
+                    break;
+
+                //per week
+                case "W":
+                    taxFree = Math.Round(11500 / week, 2);
+                    highIncome = Math.Round(45000 / week, 2);
+                    addIncome = Math.Round(150000 / week, 2);
+                    break;
+            }
+
+            /*If the user is over the age of 65, a sum of £10600 will be added to the taxfree 
+            total*/
+            string pension = (age.SelectedValue);
+            if (pension == "O")
+            {
+                taxFree = taxFree + 10600;
+            }
+
+
+            // calculating the Tax brackets
+            double taxable = income - taxFree;
+
+            double basic = Math.Round(income - (0.2 * taxable), 2);
+            double basicTax = Math.Round(0.2 * taxable, 2);
+
+            double high = Math.Round(income - (0.4 * taxable), 2);
+            double highTax = Math.Round(0.4 * taxable, 2);
+
+            double additional = Math.Round(income  - (0.45 * taxable), 2);
+            double additionalTax = Math.Round(0.45 * taxable, 2);
+
+
+            //calculating the NI according to income earnings.
+            double plusNi = 0;
+            if (ni.Checked)
+            {
+                if (income > taxFree && income < highIncome)
+                {
+                    plusNi = Math.Round(0.12 * taxable, 2);
+                }
+                else
+                {
+                    plusNi = Math.Round(0.2 * taxable, 2);
+                }
+            }
+
+            double total;
+            double totalTax;
+
+            //tax free breakdown
+            if (income <= taxFree)
+            {
+                afterTax.Text = ("£" + income + "");
+
+                gross.Text = ("" + income + "");
+                allowance.Text = ("£" + taxFree + "");
+                taxableTotal.Text = ("£" + 0 + "");
+                taxed.Text = ("£" + 0 + "");
+                niTax.Text = ("£" + 0 + "");
+                taxTotal.Text = ("£" + 0 + "");
+            }
+
+            //basic tax breakdown
+            else if (income > taxFree && income < highIncome)
+            {
+                total = basic - plusNi;
+                totalTax = basicTax + plusNi;
+
+                afterTax.Text = ("£" + total + "");
+
+                gross.Text = ("£" + income + "");
+                allowance.Text = ("£" + taxFree + "");
+                taxableTotal.Text = ("£" + taxable + "");
+                taxed.Text = ("£" + basicTax + "");
+                niTax.Text = ("£" + plusNi + "");
+                taxTotal.Text = ("£" + totalTax + "");
+            }
+
+            //high tax breakdown
+            else if (income > highIncome && income < addIncome)
+            {
+                total = high - plusNi;
+                totalTax = highTax + plusNi;
+
+                afterTax.Text = ("£" + total + "");
+
+                gross.Text = ("£" + income + "");
+                allowance.Text = ("£" + taxFree + "");
+                taxableTotal.Text = ("£" + taxable + "");
+                taxed.Text = ("£" + highTax + "");
+                niTax.Text = ("£" + plusNi + "");
+                taxTotal.Text = ("£" + totalTax + "");
+            }
+
+            //additional tax breakdown
+            else if (income > addIncome)
+            {
+                total = high - plusNi;
+                totalTax = additionalTax + plusNi;
+                afterTax.Text = ("£" + total + "");
+
+                gross.Text = ("£" + income + "");
+                allowance.Text = ("£" + taxFree + "");
+                taxableTotal.Text = ("£" + taxable + "");
+                taxed.Text = ("£" + additionalTax + "");
+                niTax.Text = ("£" + plusNi + "");
+                taxTotal.Text = ("£" + totalTax + "");
+            }
+
+        }
+        catch(Exception error)
+        {
+            errortext.Text = "Please type in your national gross income.";
+        }
+        
+    }
+
+    protected void ni_CheckedChanged(object sender, EventArgs e)
+    {
+         
+    }
+
+    protected void none_CheckedChanged(object sender, EventArgs e)
+    {
+        //noneLabel.Text = "I have no student loan debt.";
+    }
+}
